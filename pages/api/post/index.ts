dbd8d392fa05efb16952d41e3f0d510b7077b7e6
yomei <hiseica2536@gmail.com>
2021-08-19 23:33:13 +0900

    ax

--- pages/api/post/index.ts
+++ pages/api/post/index.ts
@@ -6,7 +6,6 @@ import prisma from "../../../lib/prisma";
 // Optional fields in body: content
 export default async function handle(req, res) {
   const { title, content } = req.body;
-  var ip = req.headers["x-forwarded-for"] || req.socket.remoteAddress || null;
   const session = await getSession({ req });
   const result = await prisma.post.create({
     data: {
@@ -15,5 +14,5 @@ export default async function handle(req, res) {
       author: { connect: { email: session?.user?.email } },
     },
   });
-  res.json(ip);
+  res.json(res);
 }
