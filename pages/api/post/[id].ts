d84f79cfbc226589ce8c34b7ba6c81379b6416cb
yomei <hiseica2536@gmail.com>
2021-08-20 00:11:31 +0900

    ax

--- pages/api/post/[id].ts
+++ pages/api/post/[id].ts
@@ -1,7 +1,28 @@
 import prisma from "../../../lib/prisma";
+import Cors from "cors";
+
+// CORS のミドルウェアを初期化
+const cors = Cors({
+  methods: ["GET", "HEAD"],
+});
+
+function runMiddleware(req, res, fn) {
+  return new Promise((resolve, reject) => {
+    fn(req, res, (result) => {
+      if (result instanceof Error) {
+        return reject(result);
+      }
+
+      return resolve(result);
+    });
+  });
+}
 
 // DELETE /api/post/:id
+
 export default async function handle(req, res) {
+  await runMiddleware(req, res, cors);
+
   const postId = req.query.id;
   if (req.method === "DELETE") {
     const post = await prisma.post.delete({
